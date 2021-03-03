df3a4ada794060680c6303471ffe0440b8f41586
dsuru1499 <dsuru5244@gmail.com>
2021-03-03 16:31:00 +0100

    [DSU] le 03/03/2021

---
+++ src/main/resources/public/siri-xlite-cli/precache-manifest.7f8fcfbb1dcb9a0b322c4a15dc8930e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d427a8766f4449d34b118c6355da27c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ee60cd5892f5bd75fc2",
+    "url": "./static/css/2.d5dd2e98.chunk.css"
+  },
+  {
+    "revision": "199241d4786d00597c2c",
+    "url": "./static/css/main.5c8be744.chunk.css"
+  },
+  {
+    "revision": "6ee60cd5892f5bd75fc2",
+    "url": "./static/js/2.3adace0e.chunk.js"
+  },
+  {
+    "revision": "199241d4786d00597c2c",
+    "url": "./static/js/main.20bff904.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
